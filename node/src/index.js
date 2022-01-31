73c1b0cf7eb545b1fdd6a58a628e3a1b3fbde570
Moshkin Denis <disant1998@yandex.ru>
2022-01-31 20:38:27 +0300

    update node

--- node/src/index.js
+++ node/src/index.js
@@ -1,2 +1,14 @@
-console.log("test");
-debugger;
+//const http = require("http");
+//const url = require("url");
+
+//http
+//  .createServer((request, response) => {
+//    let urlRequest = url.parse(request.url, true);
+//    //console.log(urlRequest);
+//    //console.log(request);
+//    //console.log(response);
+
+//    //response.end(`<p>${request}</p>`);
+//  })
+//  .listen(3000);
+console.log("hello12");
