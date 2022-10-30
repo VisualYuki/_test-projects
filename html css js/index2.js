ccb8cdec7eac847e73dfa8d4231341bdac1dd2cb
Moshkin Denis (Frontend) <moshkin.denis.007@yandex.ru>
2022-10-30 19:08:05 +0300

    remove userless

--- html css js/index2.js
+++ html css js/index2.js
@@ -8,14 +8,4 @@
 //  .catch((error) => {
 //    console.log(error);
 //  });
-let name ="1";
-function foo() {
-  return name;
-}
-
-let number = 12/ riba;
-console.log("in first file");
-console.log(number);
-name ="2";
-
-console.log(foo());
+console.log("in second file");
